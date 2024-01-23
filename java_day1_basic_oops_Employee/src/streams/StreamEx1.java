72d8c8e948d359ed1edcf9b0a38eb8bbd95f370e
AwareSelf <namratamarathe81@gmail.com>
2024-01-23 17:31:55 +0530

    stream - sorting emps and Items using sorted() method and using reduce() method
    stream - sorting emps and Items using sorted() method and using reduce() method

--- java_day1_basic_oops_Employee/src/streams/StreamEx1.java
+++ java_day1_basic_oops_Employee/src/streams/StreamEx1.java
@@ -75,7 +75,7 @@ public class StreamEx1 {
 			lst1.add(e2);
 			e2 = new Employee(5,"Rita",70000.0,10);
 			lst1.add(e2);
-			
+			lst1.add(new Employee(3,"Radha",70000.0,10));
 			System.out.println(lst1);
 			
 		 //list of emps -->list of empNames whose salaries are greater than 5000 
@@ -103,8 +103,123 @@ public class StreamEx1 {
 			System.out.println(lst1.stream().count()); //count of all emps
 			System.out.println(names.stream().count()); //count of emps whose sal is > 5000
 			
+			
+			Stream<Employee> lstt = lst1.stream();
+			
+			//findout first empployee in deptno 10
+			Optional<Employee> opemp = lstt.distinct().filter((Employee e)->e.getDeptno()==10).findFirst(); // lstt stream is closed with terminal operation findfirst called on it
+			if(opemp.isPresent())
+			{
+				System.out.println(opemp.get());
+			}
+			
+			Employee x = lst1.stream().distinct().filter((Employee e)->e.getDeptno()==10).findFirst().orElse(new Employee(999,"xxx",0.0,0));
+			
+			
+			
+		   String empname =  lst1.stream()
+				               .filter((Employee e)->e.getEmpSalary()>5000)
+				               .map((Employee e)->e.getEmpName())
+				               .findAny()
+				               .orElse("No Emp found matching criteriaa sal > 5000");
 		  
+		   System.out.println(empname);
+		   
+		   //https://www.baeldung.com/java-stream-sum
+		   //using reduce() method
+		   List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
+		   Integer sum = integers.stream()
+		     .reduce(0, (a, b) -> a + b);
+		   
+		   /*
+		   List<Integer> integers1 = Arrays.asList(1, 2, 3, 4, 5);
+		   Integer sum1 = integers1.stream()
+		     .reduce(0, Integer::sum);
+		   
+		   List<Integer> integers2 = Arrays.asList(1, 2, 3, 4, 5);
+		   Integer sum2 = integers2.stream()
+		     .mapToInt(Integer::intValue)
+		     .sum();
+		     */
+		   
+		   Item item1 = new Item(1, 10);
+		   Item item2 = new Item(2, 15);
+		   Item item3 = new Item(3, 25);
+		   Item item4 = new Item(4, 40);
+		   
 		  
+		   List<Item> items = Arrays.asList(item1, item2, item3, item4);
+		   
+		   /*
+		   	int summ =  0;
+		   
+		   for(Item item:items)
+		       summ = summ +item.getPrice();
+		   
+		   System.out.println(summ);
+		   */
+		           
+			   
+		   Integer sum3 = items.stream()
+				   .map(item -> item.getPrice()) //   40 25 15 10 // 40 25 25 // 40 50 //90
+				   .reduce(0, (a, b) -> a + b);
+
+		   Integer sum4 = items.stream()
+				   .map(x1 -> x1.getPrice())
+				   .reduce(0, Integer::sum);
+		   
+		   //find out total salary of all emps in deptno 10 from given list of emps
+		     double totsal = lst1.stream().filter((Employee e)->e.getDeptno() == 10)
+		                  .map((Employee e)->e.getEmpSalary()) //stream of salaries of all emps of dept 10
+		                  .reduce(0.0,(a,b)->a+b);
+		    System.out.println("Total salary of all emps of deptno 10:"+totsal);
+		    
+		    //print emp in list in ascending order of emp salaries
+		    lst1.stream()
+		         .sorted((Employee ee1,Employee ee2)->(int)(ee1.getEmpSalary() - ee2.getEmpSalary()))
+		    	 .forEach((Employee es)->System.out.println(es));
+		   
+		    //print items in ascending order of their prices
+		   items.stream() 
+	        .sorted((itm1, itm2)->itm1.getPrice() - itm2.getPrice()) 
+	        .forEach((Item itm)->System.out.println(itm));
+	      //  .forEach(System.out::println); 
 	}
 
+}
+class Item {
+
+    private int id;
+    private Integer price;
+
+    public Item(int id, Integer price) {
+        this.id = id;
+        this.price = price;
+    }
+
+    // Standard getters and setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", price=" + price + "]";
+	}
+
+  
+	
+    
 }
