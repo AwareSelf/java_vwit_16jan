975690e3094c49a83a166cc49c2b6bc767be05a6
AwareSelf <namratamarathe81@gmail.com>
2024-01-25 16:06:35 +0530

    exception handling throws clause
    exception handling throws clause

---
+++ java_day1_fulljavaproject/JavaBasicOopsNama/src/exceptionhandling/ExceptionHandlingThrowsClause.java
@@ -0,0 +1,57 @@
+package exceptionhandling;
+import java.io.*;
+import java.sql.*;
+
+public class ExceptionHandlingThrowsClause {
+
+	public static void main(String[] args)  {
+		
+		
+		//For runtime exception and its subclasses compiler will not
+		//complain (not give compiler error) if you do not handle the exception using try-catch or throws clause
+		
+		File f = new File("C:\\JavaTrack_VWIT_2024\\java_wrkspc\\JavaBasicOopsNama\\src\\exceptionhandling\\ExceptionEx.java");
+		System.out.println("program begins...");
+		
+		try {
+			func1(f);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    
+		System.out.println("remaining program...");
+
+	}
+	
+	
+	static void func1(File f) throws IOException
+	{
+	   
+				func(f);
+							
+				System.out.println("rem program in func1..");
+				
+			
+		
+	}
+	
+	
+	
+	
+	static void func(File f) throws IOException 
+	{
+		
+		
+		FileReader fr = new FileReader(f);
+		FileReader fr1 = new FileReader("abc1.java");
+		System.out.println("reading from file...");
+		fr.read();
+		fr.close();
+		fr1.close();
+		
+		
+		
+	}
+
+}
